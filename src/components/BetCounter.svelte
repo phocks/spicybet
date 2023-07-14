c4e2f0eeec3bbbd8456491f47741f719428a6309
Joshua Byrd <phocks@gmail.com>
2023-07-14 23:43:17 +1000

    Init and add a counter

---
+++ src/components/BetCounter.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  let count: number = 0;
+
+  const handleClick = () => {
+    count = count + 1;
+  };
+</script>
+
+<button on:click={handleClick}>{count}</button>
+
+<style lang="scss">
+  button {
+    margin-top: 2rem;
+    font-size: 2rem;
+    font-weight: bold;
+    color: #fff;
+    background-color: #000;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    border: none;
+  }
+</style>
