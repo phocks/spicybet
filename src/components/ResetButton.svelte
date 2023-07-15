21059008b82d96b3e5ab83533a48dd08c376b399
Joshua Byrd <phocks@gmail.com>
2023-07-16 00:22:05 +1000

    Add reset

---
+++ src/components/ResetButton.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import {
+    getDatabase,
+    ref,
+    get,
+    set,
+    onValue,
+    child,
+    push,
+    update,
+    increment,
+  } from "firebase/database";
+
+  const handleClick = () => {
+    resetCount();
+  };
+
+  function resetCount() {
+    const dbRef = ref(getDatabase());
+
+    const updates = {};
+    updates[`match/1/count`] = 0;
+    update(dbRef, updates);
+  }
+</script>
+
+<button on:click={handleClick}>Reset</button>
+
+<style lang="scss">
+  button {
+    font-size: 2rem;
+    font-weight: bold;
+    color: #fff;
+    background-color: #000;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    border: none;
+  }
+</style>
