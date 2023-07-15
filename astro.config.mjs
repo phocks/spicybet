fe13fc51376c14b3965b2bd8f3d069c67e7f43ff
Joshua Byrd <phocks@gmail.com>
2023-07-16 00:23:59 +1000

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   integrations: [svelte()],
+  adapter: vercel()
 });
