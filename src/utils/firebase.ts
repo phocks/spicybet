7d2eb300c4d8924c78108a364c255d0704071a16
Joshua Byrd <phocks@gmail.com>
2023-07-16 17:06:11 +1000

    Data consistency

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import wrap from "await-to-js";
+import { match, P } from "ts-pattern";
 
 import {
   getDatabase,
@@ -24,24 +26,41 @@ export const getFirebaseApp = () => {
 };
 
 export const getFirebaseDatabase = async (matchId) => {
+  const initialData = {
+    id: matchId,
+    round: 1,
+  };
+
   const createInitialData = async (matchId: string) => {
     const db = getDatabase();
 
-    const result = await set(ref(db, "match/" + matchId), {
-      id: matchId,
-    });
+    const [error, result] = await wrap(
+      set(ref(db, "match/" + matchId), initialData)
+    );
 
-    console.log("Initial data created", result);
+    if (!error) console.log("Initial data created");
+    else console.log("Error creating initial data", error);
   };
 
   const dbRef = ref(getDatabase());
   const snapshot = await get(child(dbRef, `match/${matchId}`));
 
   if (snapshot.exists()) {
-    console.log("Data exists. All is well...");
-    console.log(snapshot.val());
+    console.log("Data exists. Checking if consistent...");
+    const matchData = snapshot.val();
+    match(matchData)
+      .with({ id: P.string, round: P.number }, () => {
+        console.log("Data is consistent");
+      })
+      .otherwise(async () => {
+        console.log("Data is inconsistent, updating...");
+        await createInitialData(matchId);
+      });
+
+    return matchData;
   } else {
     console.log("Data not there, creating...");
     await createInitialData(matchId);
+    return initialData;
   }
 };
