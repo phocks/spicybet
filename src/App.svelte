7c452b3987a22a85bae659109e80d5a906dcbeac
Joshua Byrd <phocks@gmail.com>
2023-10-08 08:49:44 +1000

    3 cols

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,27 @@
 <script lang="ts"></script>
 
-<main>Coming soon!</main>
+<main class="layout">
+  <div class="box">1</div>
+  <div class="box">2</div>
+  <div class="box">3</div>
+</main>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .layout {
+    display: grid;
+    grid-auto-flow: row;
+    gap: 1rem;
+  }
+
+  .box {
+    padding: 1rem;
+    border: 2px solid #ccc;
+    border-radius: 1rem;
+  }
+
+  @media (min-width: 55em) {
+    .layout {
+      grid-auto-flow: column;
+    }
+  }
+</style>
