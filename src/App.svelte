4fb3678e88442120c072c5f1587e4ab5371c329c
Joshua Byrd <phocks@gmail.com>
2023-10-08 09:36:14 +1000

    3 boxes

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,15 @@
 <script lang="ts"></script>
 
 <main class="layout">
-  <div class="box">1</div>
-  <div class="box">2</div>
-  <div class="box">3</div>
+  <div class="box">
+    <h1>Player 1</h1>
+  </div>
+  <div class="box">
+    <h1>Match stats</h1>
+  </div>
+  <div class="box">
+    <h1>Player 2</h1>
+  </div>
 </main>
 
 <style lang="scss">
@@ -14,9 +20,16 @@
   }
 
   .box {
+    background-color: #333;
     padding: 1rem;
     border: 2px solid #ccc;
     border-radius: 1rem;
+  }
+
+  h1 {
+    color: #fff;
+    font-size: 1.5rem;
+    margin: 0;
   }
 
   @media (min-width: 55em) {
